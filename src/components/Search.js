25dfafc7a0ecababcc4ad72fca773cc60a28e926
unknown <adewaledavidola@gmail.com>
2023-07-03 09:35:43 +0100

    implemented the useRef hook for the search bar to improve UX

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,31 @@
+import { useEffect, useRef } from 'react';
+
 export default function Search({ query, setQuery }) {
+  // useEffect(function () {
+  //   const el = document.querySelector('.search');
+  //   console.log(el);
+  //   el.focus();
+  // }, []);
+
+  const inputElement = useRef(null);
+  useEffect(
+    function () {
+      function callback(e) {
+        inputElement.current.focus();
+        if (e.code === 'Enter') {
+          inputElement.current.focus();
+          setQuery('');
+        }
+
+        if (document.activeElement === inputElement.current) return;
+      }
+      document.addEventListener('keydown', callback);
+
+      return () => document.removeEventListener('keydown', callback);
+    },
+    [setQuery]
+  );
+
   return (
     <input
       className="search"
@@ -6,6 +33,7 @@ export default function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputElement}
     />
   );
 }
